b42b819fd6eb6a699558bee2a638eca6cb82ce1a
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2024-06-28 21:55:15 +0330

    add reducers and middleware

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,7 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
+import productApi from "@/services/api";
 
 export const makeStore = () => {
    return configureStore({
-      reducer: {},
+      reducer: {
+         [productApi.reducerPath]: productApi.reducer,
+      },
+      // Adding the api middleware enables caching, invalidation, polling,
+      // and other useful features of `rtk-query`.
+      middleware: (getDefaultMiddleware) =>
+         getDefaultMiddleware().concat(productApi.middleware),
    });
 };
