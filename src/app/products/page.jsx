33782e265b11c38ede92e8116f36c8ef8de251ba
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2024-06-28 21:54:38 +0330

    add product price

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -1,3 +1,24 @@
+"use client";
+
+import { useGetAllProductsQuery } from "@/services/api";
+
 export default function Product() {
-   return <h1 className="w-[100vw] h-[100vh]">All Product</h1>;
+   const { data, isLoading, isError } = useGetAllProductsQuery();
+
+   return (
+      <div className="p-4">
+         <h1>All Product</h1>
+         <div className="flex flex-col">
+            {isLoading && "loading.."}
+            {isError && "somthing went wrong"}
+            {data.products.map((product) => {
+               return (
+                  <div key={product.id} className="p-2">
+                     price: {product.price}$
+                  </div>
+               );
+            })}
+         </div>
+      </div>
+   );
 }
