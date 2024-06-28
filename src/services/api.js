712df272267f0013234d3e7debbfa7ccb4091e4e
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2024-06-28 22:26:08 +0330

    delete endpoints to create seperate endpoints in own folders

--- src/services/api.js
+++ src/services/api.js
@@ -1,16 +1,10 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const productApi = createApi({
-   reducerPath: "productApi",
+   reducerPath: "api",
    baseQuery: fetchBaseQuery({ baseUrl: "https://dummyjson.com/" }),
-   endpoints: (builder) => ({
-      getAllProducts: builder.query({
-         query: () => "products",
-      }),
-      getProductById: builder.query({
-         query: (productId) => `products/${productId}`,
-      }),
-   }),
+   tagTypes: ["product", "cart"],
+   endpoints: () => ({}),
 });
 
 export const { useGetAllProductsQuery, useGetProductByIdQuery } = productApi;
