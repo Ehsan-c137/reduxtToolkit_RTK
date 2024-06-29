31905d5c614d4ecbf27d78fee92ef8ac5443e296
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2024-06-29 18:54:09 +0330

    make a seperate component for All products

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -1,68 +1,30 @@
-"use client";
+import AllProducts from "@/components/Allproducts";
 
-import { useGetAllProductsQuery } from "@/services/product/productApi";
-import Image from "next/image";
-import Link from "next/link";
+async function getData() {
+   const res = await fetch(`https://dummyjson.com/products`, {
+      headers: {
+         "Content-Type": "application/json",
+      },
+   });
+   if (!res.ok) {
+      console.log(res);
+      // This will activate the closest `error.js` Error Boundary
+      throw new Error("Failed to fetch data");
+   }
 
-export default function Product() {
-   const { data, isLoading, isError } = useGetAllProductsQuery();
-   
+   return res.json();
+}
+
+export default async function Product() {
+   // const { data, isLoading, isError } = useGetAllProductsQuery();
+   const data = await getData();
    return (
       <div className="p-4">
          <h1>All Product</h1>
          <div className="flex">
-            {isLoading && "loading.."}
-            {isError && "somthing went wrong"}
-
-            <div className="flex flex-wrap w-full h-full">
-               {data?.products.map((product) => {
-                  return (
-                     <Link
-                        href={`products/${product.id}`}
-                        key={product.id}
-                        className="p-2"
-                     >
-                        <div className="flex flex-col space-y-8   animated fadeIn faster justify-center items-center inset-0 z-50 outline-none focus:outline-none bg-gray-300">
-                           <div className="block rounded-lg bg-white w-72">
-                              <div
-                                 className="relative overflow-hidden bg-cover bg-no-repeat"
-                                 data-te-ripple-init
-                                 data-te-ripple-color="light"
-                              >
-                                 <Image
-                                    width={250}
-                                    height={350}
-                                    className="rounded-lg  sm:m-h-64 md:h-64 w-full"
-                                    src={product?.images[0]}
-                                    alt={"template"}
-                                 />
-                                 <a href="#!">
-                                    <div className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-[hsla(0,0%,98%,0.15)] bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100"></div>
-                                 </a>
-                              </div>
-
-                              <div className="p-2">
-                                 <div className="flex justify-between">
-                                    <h5 className="mb-2 text-sm font-bold leading-tight text-neutral-800 dark:text-neutral-50">
-                                       {product?.title}
-                                    </h5>
-                                    <h5 className="mb-2 text-sm font-bold leading-tight text-neutral-800 dark:text-neutral-50 flex">
-                                       {product?.category}
-                                    </h5>
-                                 </div>
-                                 <p className="mb-1 text-sm text-neutral-600 dark:text-neutral-200">
-                                    {product?.description}
-                                 </p>
-                                 <h5 className="mb-2 text-sm font-bold leading-tight text-neutral-800 dark:text-neutral-50">
-                                    Price: {product?.price}
-                                 </h5>
-                              </div>
-                           </div>
-                        </div>
-                     </Link>
-                  );
-               })}
-            </div>
+            {/* {isLoading && "loading.."}
+            {isError && "somthing went wrong"} */}
+            <AllProducts data={data} />
          </div>
       </div>
    );
---
+++ src/components/Allproducts.jsx
@@ -0,0 +1,67 @@
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+const AllProducts = ({ data }) => {
+   const [isMounted, setIsMounted] = useState(false);
+
+   useEffect(() => {
+      setIsMounted(true);
+   }, []);
+
+   !isMounted && null;
+
+   return (
+      <>
+         <div className="flex flex-wrap w-full h-full">
+            {data?.products.map((product) => {
+               return (
+                  <div key={product.id} className="p-2">
+                     <div className="flex flex-col space-y-8   animated fadeIn faster justify-center items-center inset-0 z-50 outline-none focus:outline-none bg-gray-300">
+                        <div className="block rounded-lg bg-white w-72">
+                           <div
+                              className="relative overflow-hidden bg-cover bg-no-repeat"
+                              data-te-ripple-init
+                              data-te-ripple-color="light"
+                           >
+                              <Image
+                                 width={250}
+                                 height={350}
+                                 className="rounded-lg  sm:m-h-64 md:h-64 w-full"
+                                 src={product?.images[0]}
+                                 alt={"template"}
+                              />
+                              <Link href={`products/${product.id}`}>
+                                 <div className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-[hsla(0,0%,98%,0.15)] bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100"></div>
+                              </Link>
+                           </div>
+
+                           <div className="p-2">
+                              <div className="flex justify-between">
+                                 <h5 className="mb-2 text-sm font-bold leading-tight text-neutral-800 dark:text-neutral-50">
+                                    {product?.title}
+                                 </h5>
+                                 <h5 className="mb-2 text-sm font-bold leading-tight text-neutral-800 dark:text-neutral-50 flex">
+                                    {product?.category}
+                                 </h5>
+                              </div>
+                              <p className="mb-1 text-sm text-neutral-600 dark:text-neutral-200">
+                                 {product?.description}
+                              </p>
+                              <h5 className="mb-2 text-sm font-bold leading-tight text-neutral-800 dark:text-neutral-50">
+                                 Price: {product?.price}
+                              </h5>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               );
+            })}
+         </div>
+      </>
+   );
+};
+
+export default AllProducts;
