5568b7fc37d0dbcb2ed90c7f36df7cbbf8004003
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2024-06-29 20:03:20 +0330

    change style

--- src/app/products/[id]/page.jsx
+++ src/app/products/[id]/page.jsx
@@ -23,7 +23,6 @@ async function getData(id) {
 
 export default async function Page(props) {
    const data = await getData(props.params.id);
-   console.log(data);
 
    // const { data, isLoading, error } = useGetProductByIdQuery(`${params.id}`);
    // const [updateCart, { isLoading: isUpdating, error: updateCartError }] =
@@ -34,70 +33,91 @@ export default async function Page(props) {
 
    // console.log(GetByName());
 
-   // const handleClick = () => {
-   //    try {
-   //       console.log(updateCartError);
-   //       updateCart(params.id, {
-   //          products: [
-   //             {
-   //                id: 1,
-   //                quantity: 1,
-   //             },
-   //          ],
-   //       })
-   //          .unwrap()
-   //          .then((res) => {
-   //             console.log(res);
-   //             toast("cart updated");
-   //          });
-   //    } catch (err) {
-   //       toast("something went wrong!");
-   //    }
-   // };
-
    return (
-      <div className="bg-gray-300 h-[100vh]">
+      <div className="bg-gray-300 h-[100vh] p-4">
          <Toaster />
 
          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
             <Link href="/products">Back to Products</Link>
          </button>
-         <div className="flex flex-col space-y-8  animated fadeIn faster justify-center items-center inset-0 z-50 outline-none focus:outline-none ">
-            <div className="block rounded-lg bg-white w-72 mt-32">
-               <div
-                  className="relative overflow-hidden bg-cover bg-no-repeat"
-                  data-te-ripple-init
-                  data-te-ripple-color="light"
-               >
+         <div className="w-full h-full flex items-center justify-center">
+            <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+               <a href="#">
                   <Image
                      width={250}
-                     height={350}
-                     className="rounded-lg  sm:m-h-64 md:h-64 w-full"
+                     height={250}
+                     className="p-8 rounded-t-lg"
                      src={data?.images[0]}
-                     alt={"template"}
+                     alt="product image"
                   />
-                  <a href="#!">
-                     <div className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-[hsla(0,0%,98%,0.15)] bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100"></div>
-                  </a>
-               </div>
-
-               <div className="p-2">
-                  <div className="flex justify-between">
-                     <h5 className="mb-2 text-sm font-bold leading-tight text-neutral-800 dark:text-neutral-50">
+               </a>
+               <div className="px-5 pb-5">
+                  <a href="#">
+                     <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
                         {data?.title}
                      </h5>
-                     <h5 className="mb-2 text-sm font-bold leading-tight text-neutral-800 dark:text-neutral-50 flex">
-                        {data?.category}
-                     </h5>
+                  </a>
+                  <div className="flex items-center mt-2.5 mb-5">
+                     <div className="flex items-center space-x-1 rtl:space-x-reverse">
+                        <svg
+                           className="w-4 h-4 text-yellow-300"
+                           aria-hidden="true"
+                           xmlns="http://www.w3.org/2000/svg"
+                           fill="currentColor"
+                           viewBox="0 0 22 20"
+                        >
+                           <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                        </svg>
+                        <svg
+                           className="w-4 h-4 text-yellow-300"
+                           aria-hidden="true"
+                           xmlns="http://www.w3.org/2000/svg"
+                           fill="currentColor"
+                           viewBox="0 0 22 20"
+                        >
+                           <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                        </svg>
+                        <svg
+                           className="w-4 h-4 text-yellow-300"
+                           aria-hidden="true"
+                           xmlns="http://www.w3.org/2000/svg"
+                           fill="currentColor"
+                           viewBox="0 0 22 20"
+                        >
+                           <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                        </svg>
+                        <svg
+                           className="w-4 h-4 text-yellow-300"
+                           aria-hidden="true"
+                           xmlns="http://www.w3.org/2000/svg"
+                           fill="currentColor"
+                           viewBox="0 0 22 20"
+                        >
+                           <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                        </svg>
+                        <svg
+                           className="w-4 h-4 text-gray-200 dark:text-gray-600"
+                           aria-hidden="true"
+                           xmlns="http://www.w3.org/2000/svg"
+                           fill="currentColor"
+                           viewBox="0 0 22 20"
+                        >
+                           <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                        </svg>
+                     </div>
+                     <span className="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">
+                        5.0
+                     </span>
                   </div>
-                  <p className="mb-1 text-sm text-neutral-600 dark:text-neutral-200">
-                     {data?.description}
-                  </p>
-                  <h5 className="mb-2 text-sm font-bold leading-tight text-neutral-800 dark:text-neutral-50">
-                     Price: {data?.price}
-                  </h5>
+                  <p className="pt-4 pb-4">{data?.description}</p>
+                  <div className="flex items-center justify-between">
+                     <span className="text-3xl font-bold text-gray-900 dark:text-white">
+                        {data?.price}$
+                     </span>
+
+                     <AddToCart />
+                  </div>
                </div>
-               <AddToCart />
             </div>
          </div>
       </div>
