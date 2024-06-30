0cc6677f11278456b32f91c0fca4c0904a3c5ba6
ehsan-c137 <ehsanmousavi1.79@gmail.com>
2024-06-30 10:03:51 -0700

    delete some style

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Home() {
    return (
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <main className="flex flex-col items-center justify-between p-24">
          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
             <Link href="/products">Products</Link>
          </button>
