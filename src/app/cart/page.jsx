a1d0d309b52bf47f6c8a8701ef79ffb53fb00e7d
ehsan-c137 <ehsanmousavi1.79@gmail.com>
2024-06-30 10:04:01 -0700

    inital commit

---
+++ src/app/cart/page.jsx
@@ -0,0 +1,9 @@
+import Cart from "@/components/Cart"
+
+const page = ()=>{
+return <div className="flex items-center bg-gray-500 w-full h-full justify-center">
+            <Cart/>
+    </div>
+}
+
+export default page
