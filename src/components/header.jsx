0a9ee9538969bea60c017e45c3fd332b8e2372cd
ehsan-c137 <ehsanmousavi1.79@gmail.com>
2024-06-30 10:05:04 -0700

    initial commit

---
+++ src/components/header.jsx
@@ -0,0 +1,21 @@
+import Link from "next/link"
+
+const Header = ({children})=> {
+
+    return <>
+<ul className="flex items-center justify-between p-4 w-full sticky top-0 bg-gray-500 bg-opacity-10">
+            <div className="flex items-center gap-8">
+            <li><Link href={'/'}>Home</Link></li>
+            <li><Link href={'/products'}>products</Link></li>
+            <li><Link href={'/about'}>about</Link></li>
+            </div>
+            <Link href='/cart' className="p-1 pointer">
+                <img width="24" height="24" src="https://img.icons8.com/material-sharp/24/shopping-basket.png" alt="shopping-basket"/>
+            </Link>
+        </ul>
+        {children}
+    </>
+}
+
+
+export default Header
