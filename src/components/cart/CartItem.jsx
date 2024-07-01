2ed6b7b5ad4b33e944503c86ae3f3f44101ea07d
ehsan-c137 <ehsanmousavi1.79@gmail.com>
2024-07-01 15:19:40 +0330

    add spinner

--- src/components/cart/CartItem.jsx
+++ src/components/cart/CartItem.jsx
@@ -1,10 +1,10 @@
 import { useGetProductByIdQuery } from "@/services/product/productApi";
 import Image from "next/image";
+import Spinner from "../spinner";
 
 const CartItem = ({ id, quantity }) => {
   const { data, isLoading } = useGetProductByIdQuery(id);
 
-  isLoading && "isLoading";
   return (
     <li className="py-3 sm:py-4">
       <div className="flex items-center space-x-4">
@@ -19,14 +19,14 @@ const CartItem = ({ id, quantity }) => {
         </div>
         <div className="min-w-0 flex-1">
           <p className="truncate text-sm font-medium text-gray-900 dark:text-white">
-            {data?.title}
+            {isLoading ? <Spinner /> : data?.title}
           </p>
           <p className="truncate text-sm text-gray-500 dark:text-gray-400">
-            {data?.price}$ - x{quantity}
+            {isLoading ? <Spinner /> : `${data?.price}$ - x${quantity}`}
           </p>
         </div>
         <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-          ${data?.price * quantity}
+          {isLoading ? <Spinner /> : `$ ${data?.price * quantity}`}
         </div>
       </div>
     </li>
