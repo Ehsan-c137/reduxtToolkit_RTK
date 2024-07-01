fe0a5c77a22fa00c60c8e299959b283606397490
ehsan-c137 <ehsanmousavi1.79@gmail.com>
2024-07-01 14:43:20 +0330

    update import path

--- src/app/cart/page.jsx
+++ src/app/cart/page.jsx
@@ -1,9 +1,11 @@
-import Cart from "@/components/Cart"
+import Cart from "@/components/cart/Cart";
 
-const page = ()=>{
-return <div className="flex items-center bg-gray-500 w-full h-full justify-center">
-            <Cart/>
+const page = () => {
+  return (
+    <div className="flex items-center bg-gray-500 w-full h-full justify-center">
+      <Cart />
     </div>
-}
+  );
+};
 
-export default page
+export default page;
