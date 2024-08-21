f97edeead2817801626735723d7ddd0d0053341a
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2024-08-21 19:01:43 +0330

    add background white

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import StoreProvider from "./storeProvider";
-import Header from "@/components/header";
+import Header from "../components/header";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -12,7 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
    return (
       <html lang="en">
-         <body className={`${inter.className} min-h-[100vh] overflow-x-hidden`}>
+         <body
+            className={`${inter.className} min-h-[100vh] bg-white overflow-x-hidden`}
+         >
             <StoreProvider>
                <Header>{children}</Header>
             </StoreProvider>
